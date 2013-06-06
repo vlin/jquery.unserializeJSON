f49cbcafaaa70d0237e7d5738f5031ea66661a3e
vlin <vip.ljf@gmail.com>
2013-06-06 12:57:42 +0800

    rewrite the code.

--- jquery.unserializeJSON.js
+++ jquery.unserializeJSON.js
@@ -1,24 +1,19 @@
-;(function($) {
-  $.fn.unserializeJSON = function(json) {
+;(function ($) {
+  $.fn.unserializeJSON = function (json) {
     var that = this;
-
-    //reset the form
-    this[0].reset();
-    $.each(json, function(key, val) {
+    $.each(json, function (key, val) {
       var el = that.find('[name=' + key + ']');
       if (el.length > 0) {
-        if ($.isArray(val)) {
-          //uncheck the all checkboxes
-          el.prop('checked', !1);
-          $.each(val, function(i, v) {
-            el.filter('[value=' + v + ']').prop('checked', !0);
+        if(el.is('select') && el.prop('multiple')) {
+          $.each(val, function (i, v) {
+            el.find('option[value=' + v + ']')
+              .prop('selected', !0);
           });
+        } else if (el.is(':radio')){
+            el.filter('[value=' + val + ']')
+              .prop('checked', !0);
         } else {
-          if (el.is(':radio')) {
-            el.filter('[value=' + val + ']').prop('checked', !0);
-          } else {
             el.val(val);
-          }
         }
       }
     });
